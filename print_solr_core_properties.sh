fcc737d52c0e4faa0890ebe306f03f9071b56ad5
Mike Holzinger <mike.holzinger@smarsh.com>
2022-01-19 23:30:33 -0800

    Edit to MASTER_CORE_URL string in print

--- print_solr_core_properties.sh
+++ print_solr_core_properties.sh
@@ -1,6 +1,6 @@
 # pass in the CVAL to calling the script
 collection_id=$1
 
-/bin/grep -H MASTER /var/solr/instance-[1-99]/"${collection_id}"_{content,attachments,hierarchy,participants}/core.properties 2>/dev/null
-
-
+/bin/grep -H MASTER_CORE_URL \
+  /var/solr/instance-[1-99]/"${collection_id}"_{content,attachments,hierarchy,participants}/core.properties \
+  2>/dev/null

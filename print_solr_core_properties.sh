40b651a722928a3193a05be0239402eaf3584476
Mike Holzinger <mike.holzinger@smarsh.com>
2022-01-28 22:32:50 -0500

    Updates commands to print hostnames

--- print_solr_core_properties.sh
+++ print_solr_core_properties.sh
@@ -1,6 +1,7 @@
 # pass in the CVAL to calling the script
 collection_id=$1
-IFS=
+
+IFS=$'\n'
 
 results=($( /bin/grep -H MASTER_CORE_URL \
   /var/solr/instance-[1-99]/"${collection_id}"_{content,attachments,hierarchy,participants}/core.properties \
@@ -8,7 +9,7 @@ results=($( /bin/grep -H MASTER_CORE_URL \
 
 for formatted in "${results[@]}"
   do
-    echo "[`hostname`]:${formatted}"
+    echo "`hostname` ${formatted}"
   done
 
 
--- solr_service_status.sh
+++ solr_service_status.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+echo "`hostname`"
 sudo /sbin/service solr-01 status|grep -E 'solr_home|uptime'
